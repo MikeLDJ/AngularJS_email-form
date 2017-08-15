c6ae962021f7ef7e155e99f3fece31c9646e36d1
MikeLDJ <mikeu76@gmail.com>
2017-08-16 01:02:08 +0200

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name formApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the formApp
+ */
+angular.module('formApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name formApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the formApp
+ */
+angular.module('formApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
